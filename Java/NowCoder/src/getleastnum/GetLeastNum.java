a1c5963e0c49b8788db2bef978d5c689282530f9
qingfengwuyu <qingfengwuyu233@gmail.com>
2018-06-11 23:04:27 +0800

    add some

---
+++ Java/NowCoder/src/getleastnum/GetLeastNum.java
@@ -0,0 +1,19 @@
+package getleastnum;
+
+import java.util.ArrayList;
+
+/**
+ * @author : Qingfeng
+ * @date : 2018/6/10
+ */
+public class GetLeastNum {
+    public ArrayList<Integer> getLeastNum(int[] nums, int k) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (nums == null || nums.length == 0 || nums.length < k) {
+            return result;
+        }
+
+
+    }
+
+    private int getPivoit(int[] nums)
