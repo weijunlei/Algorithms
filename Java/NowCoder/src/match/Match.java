7b1286dbc5b703fb02c29146d4d8ecb62c0fb0c5
qingfengwuyu <qingfengwuyu233@gmail.com>
2018-06-29 23:23:19 +0800

    change code

---
+++ Java/NowCoder/src/match/Match.java
@@ -0,0 +1,13 @@
+package match;
+
+/**
+ * @author : Qingfeng
+ * @date : 2018/6/29
+ */
+public class Match {
+    public boolean match(char[] str, char[] pattern){
+        if (str == null || str.length == 0 || pattern.length == 0){
+
+        }
+    }
+}
