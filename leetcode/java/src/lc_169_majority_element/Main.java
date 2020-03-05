2ce5d6453f8378674adf1dc8bf94d4a707cfa1f9
qingfengwuyu233 <qingfengwuyu233@gmail.com>
2020-03-05 20:56:25 +0800

    lc 169 最多元素

---
+++ leetcode/java/src/lc_169_majority_element/Main.java
@@ -0,0 +1,9 @@
+package lc_169_majority_element;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 1, 2, 2, 2};
+        System.out.println(solution.majorityElement(nums));
+    }
+}
