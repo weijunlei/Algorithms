cb809105bd45701db8be4c338980f8f31f895aac
weijunlei <qingfengwuyu233@gmail.com>
2020-03-24 21:29:38 +0800

    add lc_344

---
+++ leetcode/java/src/lc_344_reverseString/Solution2.java
@@ -0,0 +1,16 @@
+package lc_344_reverseString;
+
+public class Solution2 {
+    public void reverseString(char[] s) {
+        if(s == null || s.length == 0){
+            return;
+        }
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right){
+            char temp = s[left];
+            s[left++] = s[right];
+            s[right--] = temp;
+        }
+    }
+}
