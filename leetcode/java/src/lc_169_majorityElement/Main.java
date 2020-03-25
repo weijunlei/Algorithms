de2bfb5ff7aa72f7b4145f24daa8ece9c502e62d
weijunlei <qingfengwuyu233@gmail.com>
2020-03-25 22:41:56 +0800

    add lc_169

---
+++ leetcode/java/src/lc_169_majorityElement/Main.java
@@ -0,0 +1,9 @@
+package lc_169_majorityElement;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] nums = {3, 3, 2};
+        System.out.println(solution.majorityElement(nums));
+    }
+}
---
+++ leetcode/java/src/lc_169_majorityElement/Solution.java
@@ -0,0 +1,20 @@
+package lc_169_majorityElement;
+
+public class Solution {
+    public int majorityElement(int[] nums){
+        int result = nums[0];
+        int count = 1;
+        for(int i = 1; i < nums.length; i++){
+            if (nums[i] == result){
+                count ++;
+            } else{
+                count --;
+                if (count == 0){
+                    result = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return result;
+    }
+}
